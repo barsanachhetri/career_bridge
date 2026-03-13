efc9c8456ad38c9b654bbbbc6f8aa832ce3dfc6c
syedusmaan <162048514+syedusmaan@users.noreply.github.com>
2026-03-13 21:02:44 +0530

    my second commit

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,8 @@ app.use("/api/upload", require("./routes/uploadRoutes"));
 
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log("Server Running");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
